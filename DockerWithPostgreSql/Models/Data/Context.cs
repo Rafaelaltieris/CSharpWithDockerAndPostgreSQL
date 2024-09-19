44d00bb5d24d78ec8bcc57ac0a4ebab186927a8a
Rafael Altieris <raphaelaltieris@gmail.com>
2024-09-19 11:52:21 -0300

    Docker with PostgreSQL

---
+++ DockerWithPostgreSql/Models/Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DockerWithPostgreSql.Models.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+    }
+}
